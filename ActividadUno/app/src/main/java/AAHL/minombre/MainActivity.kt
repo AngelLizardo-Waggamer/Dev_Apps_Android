a82c94e66b7ec65bd4ce4279775076bb8240d2d2
Ángel Lizardo <aahlizardo.britanico@gmail.com>
2025-08-16 10:56:49 -0600

    Agregar primera actividad al repo. La primera actividad fue una modificación sencilla a la plantilla original de Empty Activity.

---
+++ ActividadUno/app/src/main/java/AAHL/minombre/MainActivity.kt
@@ -0,0 +1,47 @@
+package AAHL.minombre
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import AAHL.minombre.ui.theme.MiNombreTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MiNombreTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Angel A H Lizardo",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hola $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    MiNombreTheme {
+        Greeting("Android")
+    }
+}
